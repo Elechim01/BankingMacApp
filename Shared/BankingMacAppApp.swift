9861fea5d748f76bd5dd019489193ab526e8d00e
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-11-01 16:20:45 +0100

    Initial Commit

---
+++ Shared/BankingMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingMacAppApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 01/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BankingMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
